807b8efd44e1d614e93dcd4594a74339568b1517
cooljasonmelton <cooljasonmelton>
2020-04-08 19:34:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+const createFareMultiplier = (i) => {
+    return (x) => x * i
+}
+
+const fareDoubler = (i) => {
+    return createFareMultiplier(i)(2)
+}
+
+const fareTripler = (i) => {
+    return createFareMultiplier(i)(3)
+
+}
+
+const selectDifferentDrivers = (array, f) => {
+    return f(array)
+}
+
+
+
+// selectDifferentDrivers() — This function takes two arguments, an array of drivers and 
+// either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two 
+// arguments, selectDifferentDrivers() will return either the first two drivers or the last two 
+// drivers.
